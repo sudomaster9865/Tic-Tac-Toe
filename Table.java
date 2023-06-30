e454fe530325ad3c8d9d45713a7efd311e130e8d
sudomaster9865 <98765847+sudomaster9865@users.noreply.github.com>
2023-06-30 01:03:35 -0500

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,3 +1,19 @@
 public class Table {
+    private int undefined = -1;
+    private int markedX = 1;
+    private int markedO = 0;
+    public int marking;
+    public void table() {
+        marking = undefined;
+    }
+    public int getMarking() {
+        return marking;
+    }
+    public void setMarkingX() {
+        marking = markedX;
+    }
+    public void setMarkingO() {
+        marking = markedO;
+    }
 }
-//Still under development as of 6/30/2023 - 12:27 AM
+//Still under development as of 6/30/2023 - 1:04 AM
